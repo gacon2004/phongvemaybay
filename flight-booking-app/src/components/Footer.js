eef7d5d70ce821031f5a0f364e31232eecc0ef47
gacon2004 <quangminhshop6789@gmail.com>
2025-09-04 19:59:11 +0700

    hrllo

--- flight-booking-app/src/components/Footer.js
+++ flight-booking-app/src/components/Footer.js
@@ -39,21 +39,23 @@ const Footer = () => {
             </div>
             
             {/* === KHỐI ĐỐI TÁC & CHỨNG NHẬN === */}
-            <div>
+            <div className="partners-section"> {/* Thêm className cho khối này */}
               <h4>Đối tác & Chứng nhận</h4>
               
               {/* Đối tác thanh toán */}
               <p>Đối tác thanh toán:</p>
-              <img src="https://i.ibb.co/8qgvWP0/image.png" alt="Đối tác thanh toán" width="400" style={{ marginBottom: '20px' }} />
+              <div className="payment-partner-logo"> {/* Thêm className cho div chứa logo đối tác */}
+                <img src="https://i.ibb.co/8qgvWP0/image.png" alt="Đối tác thanh toán" />
+              </div>
 
               {/* Các logo chứng nhận và hãng */}
-              <div>
-                <img src="https://www.abay.vn/_Web/_File/Images/Icons/BoCongThuong.png" alt="Bộ Công Thương" width="150" style={{ marginRight: '15px' }} />
-                <img src="https://i.ibb.co/KcfnXPwr/image.png" alt="Bộ Công an" width="150" style={{ marginRight: '15px' }} />
-                <img src="https://i.ibb.co/Lh2cF2gq/image.png" alt="Vietnam Airlines" width="150" style={{ marginRight: '15px' }} />
-                <img src="https://i.ibb.co/fdC642JN/image.png" alt="Vietjet Air" width="150" style={{ marginRight: '15px' }} />
-                <img src="https://i.ibb.co/9Hr7WKcK/image.png" alt="Bamboo Airways" width="150" style={{ marginRight: '15px' }} />
-                <img src="https://i.ibb.co/m51rMJVZ/image.png" alt="Vietravel Airlines" width="150" />
+              <div className="certification-logos"> {/* Thêm className cho div chứa các logo nhỏ */}
+                <img src="https://www.abay.vn/_Web/_File/Images/Icons/BoCongThuong.png" alt="Bộ Công Thương" />
+                <img src="https://i.ibb.co/KcfnXPwr/image.png" alt="Bộ Công an" />
+                <img src="https://i.ibb.co/Lh2cF2gq/image.png" alt="Vietnam Airlines" />
+                <img src="https://i.ibb.co/fdC642JN/image.png" alt="Vietjet Air" />
+                <img src="https://i.ibb.co/9Hr7WKcK/image.png" alt="Bamboo Airways" />
+                <img src="https://i.ibb.co/m51rMJVZ/image.png" alt="Vietravel Airlines" />
               </div>
             </div>
 
